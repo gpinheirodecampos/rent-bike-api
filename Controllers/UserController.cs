ddefc3d769191d2bc47c4368dfd411bf78dcf609
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-01 09:51:16 -0300

    Adicionando UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentAPI.Context;
+using RentBikeApi.Models;
+
+namespace RentAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            var user = _context.Users.ToList();
+
+            if (user is null) { return NotFound(); }
+
+            return user;
+        }
+    }
+}
