12006e6eafa19e5087fb8b664da6965aca05e5a9
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-06 19:49:53 -0300

    Ajustes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using RentAPI.Context;
+using RentAPI.Extensions;
+using RentAPI.Filters;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddJsonOptions(options => 
+            options.JsonSerializerOptions
+                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ApiLoggingFilter>();
 
 // Adicionando string de conexao
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -26,6 +34,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Adicionando middleware de tratamento de erros
+app.ConfigureExceptionHandler();
 
 app.UseHttpsRedirection();
 
