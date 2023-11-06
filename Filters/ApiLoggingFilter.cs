e48da421cf0e451cd8595ee8860ac354a2496637
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-06 19:48:47 -0300

    Aplicando filtros

---
+++ Filters/ApiLoggingFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RentAPI.Filters
+{
+    public class ApiLoggingFilter : IActionFilter
+    {
+        // Injecao de dependencia
+        private readonly ILogger<ApiLoggingFilter> _logger;
+        public ApiLoggingFilter(ILogger<ApiLoggingFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("## Executando -> OnActionExecuting");
+            _logger.LogInformation("##################################");
+            _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation($"Model State: {context.ModelState.IsValid}");
+            _logger.LogInformation("##################################");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("## Executando -> OnActionExecuted");
+            _logger.LogInformation("##################################");
+            _logger.LogInformation($"{DateTime.Now.ToLongTimeString()}");
+            _logger.LogInformation("##################################");
+        }
+
+    }
+}
