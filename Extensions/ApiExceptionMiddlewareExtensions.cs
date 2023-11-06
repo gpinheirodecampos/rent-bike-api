f618d320280fa051836ff4ae0ad28962d5b8062c
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-06 19:48:30 -0300

    Tratando exceções

---
+++ Extensions/ApiExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using RentAPI.Models;
+using System.Net;
+
+namespace RentAPI.Extensions
+{
+    public static class ApiExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Trace = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
