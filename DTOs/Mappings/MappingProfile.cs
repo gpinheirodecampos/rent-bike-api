0d0ee7e7f7de415edf2570f30327d90626bbecda
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-13 12:38:50 -0300

    Utilizando DTO e AutoMapper

---
+++ DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using RentAPI.Models;
+
+namespace RentAPI.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using RentAPI.Models;
+
+namespace RentAPI.DTOs
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+
+        public string? UserName { get; set; }
+
+        public string? UserEmail { get; set; }
+
+        public string? Password { get; set; }
+
+        public ICollection<Rent>? Rent { get; set; }
+    }
+}
