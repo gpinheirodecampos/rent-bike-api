d01bf53cfd85efce9f136aabaf2bb4f3ddce0399
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2023-11-13 12:38:36 -0300

    Atualizando controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RentAPI.Context;
+using RentAPI.DTOs;
 using RentAPI.Filters;
 using RentAPI.Models;
+using RentAPI.Repository;
 
 namespace RentAPI.Controllers
 {
@@ -11,66 +14,90 @@ namespace RentAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly IUnityOfWork _context;
+        private readonly IMapper _mapper;
 
-        public UserController(AppDbContext context)
+        public UserController(IUnityOfWork context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
+        // user/
         [HttpGet]
         [ServiceFilter(typeof(ApiLoggingFilter))]
-        public async Task<ActionResult<IEnumerable<User>>> GetAsync()
+        public async Task<ActionResult<IEnumerable<UserDTO>>> Get()
         {
-            var users = await _context.Users.AsNoTracking().ToListAsync();
+            var users = await _context.UserRepository.Get().ToListAsync();
+            var usersDto = _mapper.Map<List<UserDTO>>(users);
 
-            if (users is null) { return NotFound("Nao ha usuarios cadastrados."); }
+            if (usersDto is null) { return NotFound("Nao ha usuarios cadastrados."); }
 
-            return users;
+            return usersDto;
         }
 
+        // user/{id}
         [HttpGet("{id:int}", Name = "ObterUser")]
-        public async Task<ActionResult<User>> GetAsync(int id)
+        public async Task<ActionResult<UserDTO>> Get(int id)
         {
-            var user = await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == id);
+            var user = await _context.UserRepository.GetByIdAsync(u => u.UserId == id);
+            var userDto = _mapper.Map<UserDTO>(user);
 
-            if (user is null) { return NotFound("Usuario nao encontrado."); }
+            if (userDto is null) { return NotFound("Usuario nao encontrado."); }
 
-            return user;
+            return userDto;
+        }
+
+        // user/{email}
+        [HttpGet("{email}")]
+        public async Task<ActionResult<UserDTO>> Get(string email)
+        {
+            var user = await _context.UserRepository.GetUserByEmail(u => u.UserEmail == email);
+            var userDto = _mapper.Map<UserDTO>(user);
+
+            if (userDto is null) { return NotFound("Usuario nao encontrado."); }
+
+            return userDto;
         }
 
         [HttpPost]
-        public ActionResult Post(User user)
+        public async Task<ActionResult> Post(UserDTO userDto)
         {
+            var user = _mapper.Map<User>(userDto);
+
             if (user is null) { return BadRequest(); }
 
-            _context.Users.Add(user);
-            _context.SaveChanges();
+            _context.UserRepository.Add(user);
+            await _context.Commit();
+
+            var userDTO = _mapper.Map<UserDTO>(user);
 
             return new CreatedAtRouteResult("ObterUser", 
-                new { id = user.UserId }, user);
+                new { id = user.UserId }, userDTO);
         }
 
         [HttpPut("{id:int}")]
-        public ActionResult Put(int id, User user)
+        public async Task<ActionResult> Put(int id, UserDTO userDto)
         {
+            var user = _mapper.Map<User>(userDto);
+
             if (id != user.UserId) { return BadRequest(); }
 
-            _context.Entry(user).State = EntityState.Modified;
-            _context.SaveChanges();
+            _context.UserRepository.Update(user);
+            await _context.Commit();
 
-            return Ok(user);
+            return Ok(userDto);
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            var user = _context.Users.FirstOrDefault(u => u.UserId == id);
+            var user = await _context.UserRepository.GetByIdAsync(u => u.UserId == id);
 
             if (user is null) { return NotFound("Usuario nao encontrado."); }
 
-            _context.Users.Remove(user);
-            _context.SaveChanges();
+            _context.UserRepository.Delete(user);
+            await _context.Commit();
 
             return Ok();
         }
