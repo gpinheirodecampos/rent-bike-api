ba5182835bd7e6083365cab84f295f9d45df1582
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-02-08 16:00:29 -0300

    fix: migrations

---
+++ Migrations/20231121222218_corrigindo_enum.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentAPI.Migrations
+{
+    public partial class corrigindo_enum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
