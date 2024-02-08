a4f4ca77e7a630e4d3d5b979f41434c7536bccda
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-02-08 16:00:21 -0300

    feat: implements Enum

---
+++ Enums/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace RentAPI.Enums
+{
+    public class Enum
+    {
+        public enum TypeBike
+        {
+            [Description("New")]
+            New = 1,
+
+            [Description("Used")]
+            Used = 2
+        }
+    }
+}
