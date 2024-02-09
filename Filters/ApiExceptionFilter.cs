9ea7465b47bbcc16c0d5e78b2f4fdeb95f19505f
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-02-09 11:47:37 -0300

    feat: implements Global Exception Filter

---
+++ Filters/ApiExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RentAPI.Filters
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ApiExceptionFilter> _logger;
+
+        public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "Ocorreu uma exceção não tratada: Status Code 500");
+
+            context.Result = new ObjectResult("Ocorreu um problema ao tratar a sua solicitação: Status Code 500")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError,
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,10 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-        .AddJsonOptions(options => 
-            options.JsonSerializerOptions
-                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ApiExceptionFilter));
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+            
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
