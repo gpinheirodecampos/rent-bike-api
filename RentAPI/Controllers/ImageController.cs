61fa2ff393aae0ccbabe13073deb3d0a35dee9ee
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-05-15 21:07:18 -0300

    feat: implements the route address as a comment for endpoints on image controller

--- RentAPI/Controllers/ImageController.cs
+++ RentAPI/Controllers/ImageController.cs
@@ -21,6 +21,7 @@ namespace RentAPI.Controllers
             _imageService = imageService;
         }
 
+        // image/
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ImageDTO>>> Get()
         {
@@ -31,6 +32,7 @@ namespace RentAPI.Controllers
             return Ok(images);
         }
 
+        // image/{id}
         [HttpGet("{id:int}", Name = "ObterImage")]
         public async Task<ActionResult<ImageDTO>> GetById(Guid id)
         {
@@ -39,31 +41,34 @@ namespace RentAPI.Controllers
             return Ok(image);
         }
 
+        // image/
         [HttpPost]
         public async Task<ActionResult> Post(ImageDTO imageDto)
         {
-            if (imageDto is null) { return BadRequest(); }
+            if (imageDto is null) { return BadRequest("O corpo da requisição não pode ser nulo."); }
 
             await _imageService.Add(imageDto);
 
             return Ok("Imagem registrada com sucesso!");
         }
 
+        // image/{id}
         [HttpPut("{id:int}")]
         public async Task<ActionResult> Put(Guid id, ImageDTO imageDto)
         {
-            if (id != imageDto.ImageId) { return BadRequest(); }
+            if (id != imageDto.ImageId) { return BadRequest("O ID digitado no body não confere com o o ID fornecido na rota."); }
 
             await _imageService.Update(imageDto);
 
             return Ok(imageDto);
         }
 
+        // image/{id}
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> Delete(Guid id)
         {
             var image = await _imageService.GetById(id);
-            if (image is null) { return NotFound(); }
+            if (image is null) { return NotFound("Não foi encontrado uma imagem para o ID fornecido."); }
 
             await _imageService.Delete(id);
 
