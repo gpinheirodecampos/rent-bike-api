e5278ea1631f006569a621039b96210e1b2321e6
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-05-26 14:17:44 -0300

    build: allowing swagger in production environment

--- RentAPI/Program.cs
+++ RentAPI/Program.cs
@@ -95,12 +95,8 @@ builder.Services.AddScoped<ApiLoggingFilter>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Adicionando middleware de tratamento de erros
 app.ConfigureExceptionHandler();
